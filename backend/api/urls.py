8bf947cc0c9433451908fef2814e88bc4d476962
Keerthana503 <keerthi016mahesh@gmail.com>
2025-10-27 13:53:22 +0530

    feat: small changes in settings.py, create api files: serializers.py views.py urls.py

---
+++ backend/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import RegisterView, MeView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    # Auth
+    path("auth/register/", RegisterView.as_view(), name="auth_register"),
+    path("auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    # Get current user
+    path("auth/me/", MeView.as_view(), name="auth_me"),
+]
